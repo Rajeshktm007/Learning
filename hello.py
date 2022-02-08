67d89ae7e56e05b0e265db6a95e458b2b99d47d2
Rajeshktm007 <95463129+Rajeshktm007@users.noreply.github.com>
2022-02-09 03:36:07 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,2 @@
 print("HEllo buddies")
-print("Enter the first number")
-val1 = int(input())
-print("Enter the second number")
-val2 = int(input())
-res = val * val2
-print("The Solution is ", res)
+print("WELCOME to the DEVOPS SESSION")
